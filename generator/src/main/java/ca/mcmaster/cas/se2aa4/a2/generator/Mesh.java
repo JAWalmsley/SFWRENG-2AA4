0f36b306c4de98989a283b83d4fc1a2cf96942e7
Jack Walmsley <jackawalmsley@gmail.com>
2023-02-11 14:23:04 -0500

    Started creating classes for all io structs

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Mesh.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+
+import java.util.ArrayList;
+
+public class Mesh {
+    private ArrayList<Segment> segments;
+
+    public Mesh() {
+        
+    }
+
+    
+}
---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Polygon.java
@@ -0,0 +1,23 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+
+import java.util.ArrayList;
+
+public class Polygon {
+    private Vertex centroid;
+    private ArrayList<Segment> segments;
+    public Polygon(Vertex centroid) {
+        this.centroid = centroid;
+    }
+
+    public void addSegment(Segment seg) {
+        if(this.segments.size() == 0) {
+            this.segments.add(seg);
+        }
+        // Find segment with a matching vertex and add it afterward
+        for(int i = 0; i < this.segments.size(); i++) {
+            if(this.segments.get(i).getV2() == seg.getV1()) {
+                this.segments.add(i+1, seg);
+            }
+        }
+    }
+}
