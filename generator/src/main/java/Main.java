e766c80925e49f1c4b0c2b017bf1cd528703afce
mgaudet <mgaudet@Ubuntu-2AA4.myguest.virtualbox.org>
2023-02-27 19:33:49 -0500

    started F11

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -1,8 +1,16 @@
+
 import ca.mcmaster.cas.se2aa4.a2.generator.GenerateMesh;
 import ca.mcmaster.cas.se2aa4.a2.generator.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.generator.SampleData;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.HelpFormatter;
+
 
 import java.io.IOException;
 
@@ -11,6 +19,10 @@ import com.google.protobuf.DescriptorProtos.GeneratedCodeInfo;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        DefaultParser cliParser = new DefaultParser();
+        Options options = new Options();
+
         GenerateMesh generator = new GenerateMesh();
         Mesh m = generator.generatePolygonMesh(4);
         m.calculateNeighbours();
