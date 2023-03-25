61ea89dd2660791f5462cd046b7dae5e0ac09467
Jack Walmsley <jackawalmsley@gmail.com>
2023-03-25 17:59:44 -0400

    F50: Created moisture structure

---
+++ island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/moisture/MoistureProfile.java
@@ -0,0 +1,8 @@
+package ca.mcmcaster.cas.se2aa4.a2.island.moisture;
+
+import ca.mcmcaster.cas.se2aa4.a2.island.adt.Board;
+
+public interface MoistureProfile {
+    int MAX_MOISTURE = 25;
+    public void drawMoisture(Board board);
+}
