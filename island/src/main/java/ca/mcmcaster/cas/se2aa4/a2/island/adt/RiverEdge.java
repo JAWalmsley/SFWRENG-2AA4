d5c958b6259976c1540ee2afbf34329cd21f7a6e
Jack Walmsley <jackawalmsley@gmail.com>
2023-03-25 03:00:14 -0400

    F35: River flowing works

--- island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/adt/RiverEdge.java
+++ island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/adt/RiverEdge.java
@@ -1,9 +1,9 @@
 package ca.mcmcaster.cas.se2aa4.a2.island.adt;
 
 public class RiverEdge extends Edge {
-    public RiverEdge(Edge e) {
+    public RiverEdge(Edge e, int thickness) {
         super(e);
         this.colour = new Colour(0, 0, 255);
-        this.thickness = 20;
+        this.thickness = thickness;
     }
 }
--- island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/rivers/RiverGenerator.java
+++ island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/rivers/RiverGenerator.java
@@ -1,8 +1,10 @@
 package ca.mcmcaster.cas.se2aa4.a2.island.rivers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.mcmcaster.cas.se2aa4.a2.island.adt.Board;
+import ca.mcmcaster.cas.se2aa4.a2.island.adt.Colour;
 import ca.mcmcaster.cas.se2aa4.a2.island.adt.Edge;
 import ca.mcmcaster.cas.se2aa4.a2.island.adt.Point;
 import ca.mcmcaster.cas.se2aa4.a2.island.adt.RiverEdge;
@@ -13,50 +15,64 @@ import ca.mcmcaster.cas.se2aa4.a2.island.adt.Tiles.OceanTile;
 import ca.mcmcaster.cas.se2aa4.a2.island.adt.Tiles.Tile;
 
 public class RiverGenerator {
-    public void placeRivers(Board board, int numRivers){
-        for(int i = 0; i < numRivers; i++){
+    int THICKNESS_INCREMENT = 6;
+    public void placeRivers(Board board, int numRivers) {
+        for (int i = 0; i < numRivers; i++) {
             Point start;
-            while(true) {
+            while (true) {
                 start = board.getPoints().get(board.rand.nextInt(board.getPoints().size()));
-                for(Tile t: board.getNeighbourTiles(start)){
-                    if(!(t instanceof LandTile)) {
-                        continue;
+                boolean onLand = true;
+                for (Tile t : board.getNeighbourTiles(start)) {
+                    if (!(t instanceof LandTile)) {
+                        onLand = false;
                     }
                 }
-                if(board.getNeighbourPoints(start).size() == 0) {
+                if(!onLand) {
+                    // Can't break from inside the other loop, so we do this instead
+                    continue;
+                }
+
+                if (board.getNeighbourPoints(start).size() == 0) {
                     continue;
                 }
                 break;
-                
+
             }
             this.growRiver(board, start);
         }
     }
 
     public void growRiver(Board board, Point seed) {
-        while(true){
+        ArrayList<Point> visited = new ArrayList<Point>();
+        int thickness = 6;
+        outerloop:
+        while (true) {
             List<Point> potentials = board.getNeighbourPoints(seed);
-            // TODO: this is for testing, set to null
-            Point nextPoint = potentials.get(0);
-            for(Tile t : board.getNeighbourTiles(seed)) {
-                if(t instanceof OceanTile || t instanceof LakeTile) {
-                    break;
+            Point nextPoint = seed;
+            for (Tile t : board.getNeighbourTiles(seed)) {
+                if (t instanceof OceanTile || t instanceof LakeTile) {
+                    break outerloop;
                 }
             }
-            for(Point p: potentials) {
-                if(p.getElevation() < seed.getElevation() && !(p instanceof RiverPoint)) {
+            for (Point p : potentials) {
+                if(p instanceof RiverPoint && !(visited.contains(p))){
+                    thickness += THICKNESS_INCREMENT;
+                }
+                if (p.getElevation() <= nextPoint.getElevation() && !(p instanceof RiverPoint)) {
                     nextPoint = p;
                 }
             }
-            if(nextPoint == null){ 
+            if (nextPoint == seed) {
                 // TODO: Create lake here
-                break;
+                ;
+                break outerloop;
             }
             int nextIdx = board.getPoints().indexOf(nextPoint);
             board.getPoints().set(nextIdx, new RiverPoint(nextPoint));
             Edge e = board.getEdge(seed, nextPoint);
-            board.getEdges().set(board.getEdges().indexOf(e), new RiverEdge(e));
+            board.getEdges().set(board.getEdges().indexOf(e), new RiverEdge(e, thickness));
+            visited.add(nextPoint);
+            seed = nextPoint;
         }
-        
     }
 }
