aee97cf07f31bb2670f7bb63cd61d4a263ce3178
Graham Morrison <morrig14@mcmaster.ca>
2023-03-25 02:23:06 -0400

    F31: Implementing elevation and different land elevation types

---
+++ island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/elevation/Volcano.java
@@ -0,0 +1,36 @@
+package ca.mcmcaster.cas.se2aa4.a2.island.elevation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
+import ca.mcmcaster.cas.se2aa4.a2.island.adt.Board;
+import ca.mcmcaster.cas.se2aa4.a2.island.adt.Point;
+import ca.mcmcaster.cas.se2aa4.a2.island.adt.Tiles.OceanTile;
+import ca.mcmcaster.cas.se2aa4.a2.island.adt.Tiles.Tile;
+
+public class Volcano extends Elevation {
+    /**
+     * Add Elevation to vertecies
+     * @param board
+     */
+    public void addElevation(Board board) {
+        for(Tile t : board.getTiles()) {
+            if(t instanceof OceanTile) {
+                continue;
+            }
+            List<Point> points =  board.getNeighbourPoints(t);
+            
+            float x;
+            float y;
+            float distanceToCenter;
+            for(Point v : points) {
+                x = v.getX();
+                y = v.getY();
+                
+                distanceToCenter = (float) Math.sqrt(Math.pow(x - board.getWidth()/2, 2) + Math.pow(y - board.getHeight()/2, 2));
+                v.setElevation((int)(25 * (1 - distanceToCenter/(board.getWidth()/2))));
+            }
+        }
+    }
+}
