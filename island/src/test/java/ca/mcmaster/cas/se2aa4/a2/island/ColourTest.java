ddde88d96401dca61c51d42042a773b499f721ad
Jack Walmsley <jackawalmsley@gmail.com>
2023-03-26 14:21:53 -0400

    Added test for colours

---
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a2/island/ColourTest.java
@@ -0,0 +1,25 @@
+package ca.mcmaster.cas.se2aa4.a2.island;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmcaster.cas.se2aa4.a2.island.adt.Colour;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ColourTest {
+    @Test
+    public void testColour() {
+        Colour c = new Colour(255, 0, 255);
+        Structs.Property p = c.toProperty();
+        assertEquals(p.getKey(), "rgb_color");
+        assertEquals(p.getValue(), "255,0,255,255");
+    }
+}
