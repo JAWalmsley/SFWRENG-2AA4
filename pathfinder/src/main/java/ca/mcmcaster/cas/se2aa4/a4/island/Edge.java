8fb8b164dbb845f177d77b530117db250d57765e
Jack Walmsley <jackawalmsley@gmail.com>
2023-03-31 19:28:29 -0400

    Created Edge outline

---
+++ pathfinder/src/main/java/ca/mcmcaster/cas/se2aa4/a4/island/Edge.java
@@ -0,0 +1,19 @@
+public class Edge {
+    public Node node1;
+    public Node node2;
+    float weight;
+
+    public Edge(Node node1, Node node2, float weight) {
+        this.node1 = node1;
+        this.node2 = node2;
+        this.weight = weight;
+    }
+
+    public Node getNodes() {
+        return new Node[] {this.node1, this.node2};
+    }
+
+    public float getWeight() {
+        return this.weight;
+    }
+}
