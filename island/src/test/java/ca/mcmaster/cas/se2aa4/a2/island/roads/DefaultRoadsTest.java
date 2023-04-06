a73e0bea99ff1aac540bb62ae78b3039e960fd4b
Jack Walmsley <jackawalmsley@gmail.com>
2023-04-05 23:03:25 -0400

    Citytype enum

--- island/src/test/java/ca/mcmaster/cas/se2aa4/a2/island/roads/DefaultRoadsTest.java
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a2/island/roads/DefaultRoadsTest.java
@@ -19,19 +19,20 @@ import ca.mcmcaster.cas.se2aa4.a2.island.adt.Edge;
 import ca.mcmcaster.cas.se2aa4.a2.island.adt.Point;
 import ca.mcmcaster.cas.se2aa4.a2.island.islandBuilder.Configuration;
 import ca.mcmcaster.cas.se2aa4.a2.island.islandBuilder.IslandBuilder;
+import ca.mcmcaster.cas.se2aa4.a2.island.roads.CityType;
 import ca.mcmcaster.cas.se2aa4.a2.island.roads.RoadGenerator;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class DefaultRoadsTest {
     RoadGenerator rg;
     Board board;
-    int numCities = 2;
+    int numCities = 3;
 
     @BeforeEach
     void setUp() throws ParseException, IOException{
         Path file = Path.of("src/test/resources").resolve("testdata.mesh");
         String filePath = file.toString();
-        String[] args = { "-i", filePath, "-o", "none", "-d", "5"};
+        String[] args = { "-i", filePath, "-o", "none"};
         Configuration config = new Configuration(args);
 
         boolean parsedCorrectly = config.parse();
@@ -41,17 +42,17 @@ public class DefaultRoadsTest {
 
         String input = config.getOption("i", "");
         String output = config.getOption("o", "");
-        String mode = config.getOption("mode", "normal");
+        String mode = "normal";
         Structs.Mesh aMesh = new MeshFactory().read(input);
-        String shapeType = config.getOption("s", "circle");
-        String elevationType = config.getOption("e", "volcano");
-        int numLakes = Integer.valueOf(config.getOption("l", "2"));
-        String heatmapInput = config.getOption("h", "i");
-        String soilProfile = config.getOption("a", "linear");
-        int numAquifers = Integer.valueOf(config.getOption("q", "3"));
-        int numRivers = Integer.valueOf(config.getOption("r", "5"));
-        String biome = config.getOption("b", "grassland");
-        long randomSeed = Long.valueOf(config.getOption("d", String.valueOf(System.currentTimeMillis())));
+        String shapeType = "circle";
+        String elevationType = "volcano";
+        int numLakes = 1;
+        String heatmapInput = "i";
+        String soilProfile = "linear";
+        int numAquifers = 1;
+        int numRivers = 0;
+        String biome = "grassland";
+        long randomSeed = 5;
         System.out.println("Using " + randomSeed + " as seed.");
 
         board = new Board(aMesh, randomSeed);
@@ -64,7 +65,7 @@ public class DefaultRoadsTest {
     void testNumCities() {
         HashSet<Point> foundCities = new HashSet<>();
         for (Point p : board.getPoints()) {
-            if(p.isCity()) {
+            if(!(p.getCity() == CityType.NONE)) {
                 foundCities.add(p);
             }
         }
