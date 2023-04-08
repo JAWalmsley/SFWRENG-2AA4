6906ff11b247aa7ca2d4ad3280edd4712ea1c032
Jack Walmsley <jackawalmsley@gmail.com>
2023-04-08 11:56:45 -0400

    Adding UML diagrams

--- island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/roads/RoadFactory.java
+++ island/src/main/java/ca/mcmcaster/cas/se2aa4/a2/island/roads/RoadFactory.java
@@ -2,6 +2,7 @@ package ca.mcmcaster.cas.se2aa4.a2.island.roads;
 
 public class RoadFactory {
     public static RoadGenerator create(int numCities) {
+        // Currently only one road type, but could be extended in future to have other types such as realistic roads
         return new DefaultRoads(numCities);
     }
 }
